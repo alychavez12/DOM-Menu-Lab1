d4fc48e86888a89dca32d7b4238ed2fdf9340b53
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2022-12-07 13:30:24 -0500

    feat: completes task 1.3

--- js/script.js
+++ js/script.js
@@ -6,12 +6,46 @@ const mainEl = document.querySelector('main');
 //console.log(mainEl);
 
 //Task 1.1
-//Set the background color of mainElt o the value stored in the --main-bgCSS custom property.
+//Set the background color of mainElt o the value stored in the --main-bg CSS custom property.
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
 
 //Task 1.2
-//Set the content of mainElto <h1>SEI Rocks!</h1>.
+//Set the content of mainEl to <h1>SEI Rocks!</h1>.
 
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 
+//Task 1.3
+//Add a class of flex-ctr to mainEl.
+
+mainEl.classList.add('flex-ctr');
+
+//Task 2.0
+//Select and cache the <nav id="top-menu">element in a variable named topMenuEl.
+
+
+//Task 2.1
+//Set the height topMenuElelement to be 100%.
+
+
+//Task 2.2
+//Set the background color of topMenuElto the value stored in the --top-menu-bgCSS custom property.
+
+
+//Task 2.3
+//Add a class of flex-aroundto topMenuEl.
+
+//Task 3.0
+//Copy the following data structure to the top of script.js:
+
+
+//Task 3.1
+//Iterate over the entire menuLinksarray and for each "link" object:
+
+//Create an <a>element.
+
+//On the new element, add an hrefattribute with its value set to the hrefproperty of the "link" object.
+
+//Set the new element's content to the value of the textproperty of the "link" object.
+
+//Append the new element to the topMenuElelement.
